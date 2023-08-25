3667155152a2428f763343b956bcb10b1445aac7
Ci <15597293038@163.com>
2023-08-25 09:43:31 +0800

    master

--- models/tag_model.go
+++ models/tag_model.go
@@ -1,7 +1,8 @@
 package models
+
 //定义文章标签模型
-type Tagmodel struct{
+type Tagmodel struct {
 	MODEL
-	Title string             `gorm:"size:16" json:"title"`    //标签名称
-	Article []ArticleModel   `gorm:"many2many:article_tag_models" json:"-"` //关联该标签的文章列表
+	Title   string         `gorm:"size:16" json:"title"`                  //标签名称
+	Article []ArticleModel `gorm:"many2many:article_tag_models" json:"-"` //关联该标签的文章列表
 }
